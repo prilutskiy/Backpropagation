d903dddaa0f2814508b467265a9094094c5ee710
JoOice <maks.prilutskiy@gmail.com>
2014-10-17 14:06:53 +0300

    Added NeuralLayer class

---
+++ Backpropagation.Core/NeuralLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backpropagation.Core
+{
+    /// <summary>
+    /// Class represents artificial neural network layer
+    /// </summary>
+    /// <typeparam name="T">Type of input value</typeparam>
+    public class NeuralLayer<T>
+    {
+        private readonly int _inputCount;
+        private readonly int _outputCount;
+        private T[] Inputs;
+        public NeuralLayer(int inputCount, int outputCount, Func<T[], T> activationFunc, T[] inputs)
+        {
+            _inputCount = inputCount;
+            _outputCount = outputCount;
+
+            Inputs = inputs;
+            GenerateNeurons(_outputCount, activationFunc, inputs);
+        }
+        private void GenerateNeurons(int outputCount, Func<T[], T> activationFunc, T[] inputs)
+        {
+            for (int i = 0; i < outputCount; i++)
+            {
+                var n = new Neuron<T>(outputCount, activationFunc, inputs);
+                Neurons.Add(n);
+            }
+        }
+
+        private ICollection<Neuron<T>> Neurons { get; set; }
+
+        public T[] GetOutputs()
+        {
+            var arr = new T[_outputCount];
+            int i = 0;
+            foreach (var neuron in Neurons)
+            {
+                arr[i] = neuron.Output;
+            }
+            return arr;
+        }
+    }
+}
--- Backpropagation.Core/Neuron.cs
+++ Backpropagation.Core/Neuron.cs
@@ -15,40 +15,36 @@ namespace Backpropagation.Core
         private Func<T[], T> ActivationFunction { get; set; }
         private Double[] WeightFactors { get; set; }
         private T _output;
-        private int _inputCount;
+        private readonly int _inputCount;
         private void InitWeightFactors()
         {
             var r = new Random();
             for (int i = 0; i < WeightFactors.Count(); i++)
                 WeightFactors[i] = ((double) r.Next(-5, 5))/10.0;
         }
+
         /// <summary>
         /// Default ctor for creating neuron instance
         /// </summary>
         /// <param name="inputCount">Count of neuon inputs</param>
         /// <param name="activationFunc">Neuron activation function</param>
-        public Neuron(int inputCount, Func<T[],T> activationFunc)
+        /// <param name="inputs">Neuron inputs</param>
+        public Neuron(int inputCount, Func<T[],T> activationFunc, T[] inputs)
         {
             _inputCount = inputCount;
-            Inputs = new T[inputCount];
+
+            Inputs = inputs;
             WeightFactors = new double[inputCount];
             ActivationFunction = activationFunc;
             InitWeightFactors();
         }
 
-        public void InitializeInputs(T[] inputArray)
-        {
-            for (int i = 0; i < inputArray.Count(); i ++)
-                this.Inputs[i] = inputArray[i];
-        }
         public T[] Inputs { get; private set; }
 
         virtual public T Output
         {
             get 
             {
-                if (Inputs.Count()!= _inputCount) 
-                    throw new ArgumentOutOfRangeException("Input count does not match the value specified");
                 _output = ActivationFunction(Inputs);
                 return _output;
             }
